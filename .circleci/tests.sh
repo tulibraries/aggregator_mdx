3acc19caa09cdb5cb0382fbf10d7b1224936c844
cmharlow <cmharlow@gmail.com>
2019-11-12 16:23:21 -0500

    migrate from travis to circleci

--- .circleci/tests.sh
+++ .circleci/tests.sh
@@ -2,12 +2,12 @@
 set -e
 
 for xspectest in $(ls tests/xslt/*.xspec); do \
-docker-compose run xspec "/$xspectest" &> .travis/result.log;
-    if grep -q ".*failed:\s[1-9]" .travis/result.log || grep -q -E "\*+\sError\s(running|compiling)\sthe\stest\ssuite" .travis/result.log;
+docker run xspec "$xspectest" &> .circleci/result.log;
+    if grep -q ".*failed:\s[1-9]" .circleci/result.log || grep -q -E "\*+\sError\s(running|compiling)\sthe\stest\ssuite" .circleci/result.log;
         then
             echo "FAILED: $xspectest";
             echo "---------- result.log";
-            cat .travis/result.log;
+            cat .circleci/result.log;
             echo "----------";
             exit 1;
         else echo "OK: $xspectest";
@@ -15,12 +15,12 @@ docker-compose run xspec "/$xspectest" &> .travis/result.log;
 done
 
 for xspectest in $(ls tests/schematron/*.xspec); do \
-docker-compose run xspec -s "/$xspectest" &> .travis/result.log;
-    if grep -q ".*failed:\s[1-9]" .travis/result.log || grep -q -E "\*+\sError\s(running|compiling)\sthe\stest\ssuite" .travis/result.log;
+docker run xspec -s "$xspectest" &> .circleci/result.log;
+    if grep -q ".*failed:\s[1-9]" .circleci/result.log || grep -q -E "\*+\sError\s(running|compiling)\sthe\stest\ssuite" .circleci/result.log;
         then
             echo "FAILED: $xspectest";
             echo "---------- result.log";
-            cat .travis/result.log;
+            cat .circleci/result.log;
             echo "----------";
             exit 1;
         else echo "OK: $xspectest";
