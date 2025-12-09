d2d94d3eea7d3792a0163e01aef22445baea682c
Steven Ng <steven.ng@temple.edu>
2025-12-09 14:13:54 -0700

    Fixes error info not passed back to user

--- .github/scripts/tests.sh
+++ .github/scripts/tests.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 set -euo pipefail
 
-for xspectest in $(ls tests/xslt/*.xspec); do \
-docker run xspec "$xspectest" &> .github/result.log;
-    if grep -Eq 'failed:[[:space:]]*[1-9]' .github/result.log || grep -Eq '\*+ Error (running|compiling) the test suite' .github/result.log;
-      then
-          echo "FAILED: $xspectest";
-          echo "---------- result.log";
-          cat .github/result.log;
-          echo "----------";
-          exit 1;
-      else echo "OK: $xspectest";
+for xspectest in tests/xslt/*.xspec; do
+    if ! docker run xspec "$xspectest" > .github/result.log 2>&1; then
+        exit_code=$?
+    else
+        exit_code=0
+    fi
+
+    if [ $exit_code -ne 0 ] || grep -Eq 'failed:[[:space:]]*[1-9]' .github/result.log || grep -Eq '\*+ Error (running|compiling) the test suite' .github/result.log; then
+        echo "FAILED: $xspectest"
+        echo "---------- result.log"
+        cat .github/result.log
+        echo "----------"
+        [ $exit_code -ne 0 ] && echo "docker exit code: $exit_code"
+        exit 1
+    else
+        echo "OK: $xspectest"
     fi
 done
 
-for xspectest in $(ls tests/schematron/*.xspec); do \
-docker run xspec -s "$xspectest" &> .github/result.log;
-    if grep -Eq 'failed:[[:space:]]*[1-9]' .github/result.log || grep -Eq '\*+ Error (running|compiling) the test suite' .github/result.log;
-        then
-            echo "FAILED: $xspectest";
-            echo "---------- result.log";
-            cat .github/result.log;
-            echo "----------";
-            exit 1;
-        else echo "OK: $xspectest";
+for xspectest in tests/schematron/*.xspec; do
+    if ! docker run xspec -s "$xspectest" > .github/result.log 2>&1; then
+        exit_code=$?
+    else
+        exit_code=0
+    fi
+
+    if [ $exit_code -ne 0 ] || grep -Eq 'failed:[[:space:]]*[1-9]' .github/result.log || grep -Eq '\*+ Error (running|compiling) the test suite' .github/result.log; then
+            echo "FAILED: $xspectest"
+            echo "---------- result.log"
+            cat .github/result.log
+            echo "----------"
+            [ $exit_code -ne 0 ] && echo "docker exit code: $exit_code"
+            exit 1
+        else
+            echo "OK: $xspectest"
     fi
 done
